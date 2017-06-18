6de40081c38ed07eaab1feca4cb29a03bedabeb3
Taillook <jousoku@gmail.com>
2017-06-18 18:03:11 +0900

    [Update] Delete Comments

--- PyNews/main.py
+++ PyNews/main.py
@@ -5,6 +5,7 @@ import requests
 from html.parser import HTMLParser
 import sys
 
+
 class Parser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
@@ -16,29 +17,27 @@ class Parser(HTMLParser):
         attrs = dict(attrs)
         if tag == "h2" and "class" in attrs and attrs['class'] == "esc-lead-article-title":
             self.data.append({})
-            #print("開始タグ :", tag, attrs)
             self.title = True
             self.link = True
+
         if tag == "a" and self.link == True:
-            #print("開始タグ :", tag, attrs)
-            self.data[-1].update({"link" : attrs["href"]})
-            #print("Link : ", attrs["href"])
-    #def handle_endtag(self, tag):
-        #print("終了タグ :", tag)
+            self.data[-1].update({"link": attrs["href"]})
+
     def handle_data(self, data):
         if self.title == True or self.link == True:
-            self.data[-1].update({"title" : data})
-            #print("データ:", data)
+            self.data[-1].update({"title": data})
             self.title = False
             self.link = False
 
+
 def main(args=sys.argv[0]):
-    payload = {'topic' : 't'}
+    payload = {'topic': 't'}
     r = requests.get('https://news.google.com/news/section', params=payload)
     parser = Parser()
     parser.feed(r.text)
     parser.close()
     print(parser.data)
+
 
 if __name__ == '__main__':
     main()
